1e0a9127c392d8dd6ca003bcf51712a99c733011
francode-taype <francotaypedev@gmail.com>
2025-03-12 13:15:20 -0500

    initial setup and basic project structure

---
+++ src/main/java/dev/francode/pushNotifications/PushNotificationsApplication.java
@@ -0,0 +1,13 @@
+package dev.francode.pushNotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/francode/pushNotifications/PushNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.francode.pushNotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
